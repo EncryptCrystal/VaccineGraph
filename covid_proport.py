e9d0f53c049e686449497a276fad5ffaa9f9cc1a
Cyprien Méjat <adriendubois1411@gmail.com>
2021-07-24 20:49:38 +0200

    Commentaires complets

--- covid_proport.py
+++ covid_proport.py
@@ -26,7 +26,7 @@ def format_date(date):
     elif date[1] == "02": new_date += " Fev"
     elif date[1] == "03": new_date += " Mar"
     elif date[1] == "04": new_date += " Avr"
-    elif date[1] == "05": new_date += " Mai"                            #En fonction de la valeur de MM (nombre), on rajoute rajoute la valeur MMM (lettres) correspondante
+    elif date[1] == "05": new_date += " Mai"                            #En fonction de la valeur de MM (nombre),on rajoute la valeur MMM (lettres) correspondante
     elif date[1] == "06": new_date += " Jun"
     elif date[1] == "07": new_date += " Jul"
     elif date[1] == "08": new_date += " Aou"
@@ -73,27 +73,30 @@ def Importation(nom_fichier):
     cumul_primo_injections_50_ans = 0
     
     for donnes in table:
-      
+        #Afin de faciliter la compréhension du code, les 4 colonnes sont assignés à des variables
         date = donnes[0]
         age = donnes[1]
         primo_injections = donnes[2]
         injections_completes = donnes[3]
 
-
+        #Dans le cas où la ligne conserne les injections tout âge confondus...
         if age == 0:
             primo_injections_totales.append(primo_injections/obj_1_dose*100)
             injections_completes_totales.append(injections_completes/obj_tot_dose*100)
             liste_dates.append(format_date(date))
-      
+
+        #Dans le cas où la ligne conserne les injections de personnes entre 18 et 49 ans...
         elif 18 <= age <= 49:
             cumul_primo_injections_18_ans += primo_injections
             cumul_injections_completes_18_ans += injections_completes
-      
+
+        #Dans le cas où la ligne conserne les injections de personnes entre 50 et 79 ans...
         elif 50 <= age <= 79:
             cumul_primo_injections_50_ans += primo_injections
             cumul_primo_injections_18_ans += primo_injections
             cumul_injections_completes_18_ans += injections_completes
-      
+
+        #Dans le cas où la ligne conserne les injections de personnes de plus de 80 ans...
         elif age == 80:
             cumul_primo_injections_50_ans += primo_injections
             primo_injections_50_ans.append(cumul_primo_injections_50_ans/pop_50_ans/obj_50_ans_1_dose*100)
@@ -105,12 +108,13 @@ def Importation(nom_fichier):
             injections_completes_18_ans.append(cumul_injections_completes_18_ans/pop_18_ans/obj_18_ans_tot_dose*100)
             cumul_primo_injections_18_ans = 0
             cumul_injections_completes_18_ans = 0
-        
+    
+    #Sert à la création des dates ultérieurs à celles des données
     date_limite = str(liste_dates[-1])
-    while liste_dates[-1][0:2] != "31" and liste_dates[-1][3:7] == "Juil":
-        liste_dates.append(str(int(liste_dates[-1][0:2])+1)+" Juil")
+    while liste_dates[-1][0:2] != "31" and liste_dates[-1][3:7] == "Jul":
+        liste_dates.append(str(int(liste_dates[-1][0:2])+1)+" Jul")
             
-    liste_dates.append("01 Aou")
+    if "01 Aou" not in liste_dates: liste_dates.append("01 Aou")
         
     while int(liste_dates[-1][0:2]) < 9 and liste_dates[-1][3:6] == "Aou":
         liste_dates.append("0"+str(int(liste_dates[-1][0:2])+1)+" Aou")
@@ -118,18 +122,20 @@ def Importation(nom_fichier):
     while liste_dates[-1][0:2] != "31" and liste_dates[-1][3:6] == "Aou":
         liste_dates.append(str(int(liste_dates[-1][0:2])+1)+" Aou")
     
+    
+    #Sert à la projection des courbes
     def projectionObjectif(fonction):
         projection = list(fonction)
-        coeff =  (fonction[-1]-fonction[-8])/7
+        coeff =  (fonction[-1]-fonction[-8])/7                          #Evolution de la courbe calculé à partir des 7 derniers jours
         while len(liste_dates) != len(projection):
             projection.append(projection[-1]+coeff)
         return projection
     
     #Début de la contruction du graphique
-    plt.figure(figsize=(45,8))                      #Définit une dimension en 45/9 à cause de la grande quantité de dates
-    plt.tick_params(axis = 'x', rotation = 80)      #Tourne les dates à 80° afin qu'elles restent visibles
+    plt.figure(figsize=(45,8))                                          #Définit une dimension en 45/9 à cause de la grande quantité de dates
+    plt.tick_params(axis = 'x', rotation = 80)                          #Tourne les dates à 80° afin qu'elles restent visibles
     
-    plt.axhline(y=100,color='gray',linestyle='--')  #Trace une ligne de pointillé verticale au niveau des 100%
+    plt.axhline(y=100,color='gray',linestyle='--')                      #Trace une ligne de pointillé verticale au niveau des 100%
     
     #Trace les courbes
     plt.plot(liste_dates, projectionObjectif(primo_injections_totales), "red", label = f"Objectif de primo-vaccinés ({int(obj_1_dose/1000000)} M)")
@@ -147,8 +153,8 @@ def Importation(nom_fichier):
     plt.yticks(np.arange(0, 110, 10))
     plt.ylim(0, 110)
     
-    plt.legend()                                    #Affiche les légendes associés à la courbe correspondante
-    plt.margins(0, 0)                               #Force la disparition des marge intérieures
+    plt.legend()                                                        #Affiche les légendes associés à la courbe correspondante
+    plt.margins(0, 0)                                                   #Force la disparition des marge intérieures
     
     #Défini les titres du graphes et des axes x et y
     plt.title(f"Etat des objectifs gouvernementaux pour la fin aout (Données du {format_date(nom_fichier[12:22])} à {nom_fichier[23:28]})")
