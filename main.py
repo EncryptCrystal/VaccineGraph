73b6f40d7a1db7a330ee347d952a433cdf530207
Cyprien Méjat <adriendubois1411@gmail.com>
2021-07-29 23:37:17 +0200

    Ajout de la variable limite_date_debut

--- main.py
+++ main.py
@@ -4,42 +4,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+nom_fichier = "vacsi-a-fra-2021-07-29-19h05.csv"                                #Nom du fichier de données à traiter
+limite_date_debut = "2021-06-01"                                                #Indique à partir de quelle date commence l'origine des données en format AAAA-MM-JJ (0 pour lever limite)
+limite_jour = 2                                                                 #Indique le nombre de dates à inscrire sur l'axe des abscisses (0 ou 1 conserve la liste)
+
+
 #Liste des objectifs
-obj_1_dose = 50000000                                                   #50 000 000 primo-vaccinés
-obj_tot_dose = 35000000                                                 #35 000 000 vaccinés
-obj_50_ans_1_dose = 0.85                                                #85% des +50 ans primo-vaccinés
-obj_18_ans_1_dose = 0.75                                                #75% des +18 ans primo-vaccinés
-obj_18_ans_tot_dose = 0.66                                              #66% des +18 ans complétement vaccinés
+obj_1_dose = 50000000                                                           #50 000 000 primo-vaccinés
+obj_tot_dose = 35000000                                                         #35 000 000 vaccinés
+obj_50_ans_1_dose = 0.85                                                        #85% des +50 ans primo-vaccinés
+obj_18_ans_1_dose = 0.75                                                        #75% des +18 ans primo-vaccinés
+obj_18_ans_tot_dose = 0.66                                                      #66% des +18 ans complétement vaccinés
 
 #Données sur la population (Insee, 2021) (https://www.insee.fr/fr/outil-interactif/5367857/details/20_DEM/21_POP/21C_Figure3#)
-pop_50_ans = 27824662                                                   #27 824 662 Français ont plus de 50 ans
-pop_18_ans = 53761464                                                   #53 761 464 Français ont plus de 18 ans
-
-nom_fichier = "vacsi-a-fra-2021-07-29-19h05.csv"                        #Nom du fichier de données à traiter
-limite_jour = 0                                                         #Indique le nombre de dates à inscrire sur l'axe des abscisses (0 ou 1 conserve la liste)
+pop_50_ans = 27824662                                                           #27 824 662 Français ont plus de 50 ans
+pop_18_ans = 53761464                                                           #53 761 464 Français ont plus de 18 ans
 
 
 #Sert à limiter une liste à nb_element de manière uniforme
 def reduction(liste):
-    if limite_jour == 0 or limite_jour == 1: return liste               #nb_element ne doit pas être égal à 0 ou 1
+    if limite_jour == 0 or limite_jour == 1: return liste                       #nb_element ne doit pas être égal à 0 ou 1
     liste_compressee = []
-    coeff = len(liste)/(limite_jour-1)                                  #Calcule l'écart idéal entre 2 éléments de la liste à compresser (prends en compte le premier et dernier élément)
-    liste_compressee.append(liste[0])                                   #Ajoute le premier élement de la liste à compresser
+    coeff = len(liste)/(limite_jour-1)                                          #Calcule l'écart idéal entre 2 éléments de la liste à compresser (prends en compte le premier et dernier élément)
+    liste_compressee.append(liste[0])                                           #Ajoute le premier élement de la liste à compresser
     for i in range(len(liste)):
-        if int(i/coeff) == len(liste_compressee):                       #Si la position de l'élément est supérieure ou égale à sa position dans la liste compressée
-            liste_compressee.append(liste[i-1])                         #Alors ajouter l'élement à la liste compressée
-    liste_compressee.append(liste[-1])                                  #Ajoute le dernier élement de la liste dans la liste à compresser
+        if int(i/coeff) == len(liste_compressee):                               #Si la position de l'élément est supérieure ou égale à sa position dans la liste compressée
+            liste_compressee.append(liste[i-1])                                 #Alors ajouter l'élement à la liste compressée
+    liste_compressee.append(liste[-1])                                          #Ajoute le dernier élement de la liste dans la liste à compresser
     return liste_compressee
 
 #Sert à formater les dates : "AAAA-MM-JJ" -> "JJ MMM"
-def format_date(date):
-    date = date.rsplit("-")                                             #Sépare la chaine en une liste de 3 éléments : [AAAA, MM, JJ]
-    new_date = date[2]                                                  #Prend la valeur des jours
+def formatDate(date):
+    date = date.rsplit("-")                                                     #Sépare la chaine en une liste de 3 éléments : [AAAA, MM, JJ]
+    new_date = date[2]                                                          #Prend la valeur des jours
     if date[1] == "01": new_date += " Jan"
     elif date[1] == "02": new_date += " Fev"
     elif date[1] == "03": new_date += " Mar"
     elif date[1] == "04": new_date += " Avr"
-    elif date[1] == "05": new_date += " Mai"                            #En fonction de la valeur de MM (nombre),on rajoute la valeur MMM (lettres) correspondante
+    elif date[1] == "05": new_date += " Mai"                                    #En fonction de la valeur de MM (nombre),on rajoute la valeur MMM (lettres) correspondante
     elif date[1] == "06": new_date += " Juin"
     elif date[1] == "07": new_date += " Juill"
     elif date[1] == "08": new_date += " Aou"
@@ -58,25 +60,31 @@ def projectionObjectif(fonction):
     return projection
 
 
-fichier = open(nom_fichier,"r")                                         #Ouvre le fichier
+fichier = open(nom_fichier,"r")                                                 #Ouvre le fichier
 ligne_descripteurs = fichier.readline()
-lst_descripteurs = ligne_descripteurs.rstrip().rsplit(";")              #Sépare la première ligne (titres des colonnes) du reste des valeurs numériques
-lignes = fichier.readlines()                                            #Le reste est entreposée dans "lignes"
+lst_descripteurs = ligne_descripteurs.rstrip().rsplit(";")                      #Sépare la première ligne (titres des colonnes) du reste des valeurs numériques
+lignes = fichier.readlines()                                                    #Le reste est entreposée dans "lignes"
 table = []
+
+if limite_date_debut == 0: suppressionDate = False
+else: suppressionDate = True
+
 for ligne in lignes:
     lst = ligne.rstrip().split(";")
-    del lst[0]                                                          #Supression des valeurs du pays de l'injection (toutes dans le fichier sont en France)
-    lst[0] = int(lst[0])                                                #Conversion de l'âge des vaccinés en nombre entier (de base une chaine de caractères.)
-    del lst[2]                                                          #Suppression des primo-injections quotidiennes
-    del lst[2]                                                          #Suppression des injections complètes quotidiennes
-    lst[2] = int(lst[2])                                                #Conversion du cumul des primo-injections en nombre entier
-    lst[3] = int(lst[3])                                                #Conversion du cumul des injections complètes en nombre entier
-    del lst[4]                                                          #Suppression du taux de primo-vaccinés
-    del lst[4]                                                          #Suppression du taux de vaccinés
+    del lst[0]                                                                  #Supression des valeurs du pays de l'injection (toutes dans le fichier sont en France)
+    lst[0] = int(lst[0])                                                        #Conversion de l'âge des vaccinés en nombre entier (de base une chaine de caractères.)
+    del lst[2]                                                                  #Suppression des primo-injections quotidiennes
+    del lst[2]                                                                  #Suppression des injections complètes quotidiennes
+    lst[2] = int(lst[2])                                                        #Conversion du cumul des primo-injections en nombre entier
+    lst[3] = int(lst[3])                                                        #Conversion du cumul des injections complètes en nombre entier
+    del lst[4]                                                                  #Suppression du taux de primo-vaccinés
+    del lst[4]                                                                  #Suppression du taux de vaccinés
     table.append(lst)
-fichier.close()                                                         #Ferme le fichier
-table = sorted(table, key=itemgetter(1, 0))                             #Tri les données par date, puis par âge
+fichier.close()                                                                 #Ferme le fichier
+table = sorted(table, key=itemgetter(1, 0))                                     #Tri les données par date, puis par âge
+print(table[0][1])
 
+while suppressionDate and table[0][1] != limite_date_debut: del table[0]        #Tant que la date limite n'est pas atteinte, continuer de supprimer les données
 
 #Initialisation des variables des dates et des 5 autres courbes
 liste_dates = []
@@ -102,7 +110,7 @@ for donnes in table:
     if age == 0:
         primo_injections_totales.append(primo_injections/obj_1_dose*100)
         injections_completes_totales.append(injections_completes/obj_tot_dose*100)
-        liste_dates.append(format_date(date))
+        liste_dates.append(formatDate(date))
 
     #Dans le cas où la ligne concerne les injections de personnes entre 18 et 49 ans...
     elif 18 <= age <= 49:
@@ -128,7 +136,7 @@ for donnes in table:
         cumul_primo_injections_18_ans = 0
         cumul_injections_completes_18_ans = 0
 
-date_limite = str(liste_dates[-1])                                      #Sauvegarde de la dernière date des données
+date_limite = str(liste_dates[-1])                                              #Sauvegarde de la dernière date des données
 
 #Sert à la création des dates ultérieurs à celles des données
 while liste_dates[-1][0:2] != "31" and liste_dates[-1][3:9] == "Juill":
@@ -142,14 +150,14 @@ while int(liste_dates[-1][0:2]) < 9 and liste_dates[-1][3:6] == "Aou":
 while liste_dates[-1][0:2] != "31" and liste_dates[-1][3:6] == "Aou":
     liste_dates.append(str(int(liste_dates[-1][0:2])+1)+" Aou")
     
-liste_dates_reduite = reduction(liste_dates)                            #Reduit la liste de dates tout en conservant l'original
+liste_dates_reduite = reduction(liste_dates)                                    #Reduit la liste de dates tout en conservant l'original
 
 
 #Début de la contruction du graphique
-plt.figure(figsize=(45,8))                                              #Définit une dimension en 45/9 à cause de la grande quantité de dates
-plt.tick_params(axis = 'x', rotation = 80)                              #Tourne les dates à 80° afin qu'elles restent visibles
+plt.figure(figsize=(45,8))                                                      #Définit une dimension en 45/9 à cause de la grande quantité de dates
+plt.tick_params(axis = 'x', rotation = 80)                                      #Tourne les dates à 80° afin qu'elles restent visibles
     
-plt.axhline(y=100,color='gray',linestyle='--')                          #Trace une ligne de pointillé verticale au niveau des 100%
+plt.axhline(y=100,color='gray',linestyle='--')                                  #Trace une ligne de pointillé verticale au niveau des 100%
 
 #Trace les courbes
 plt.plot(reduction(liste_dates_reduite), reduction(projectionObjectif(primo_injections_totales)), "red", label = f"Objectif de primo-vaccinés ({int(obj_1_dose/1000000)} M)")
@@ -167,11 +175,11 @@ plt.axvline(x=liste_dates[-1], color='gray', linestyle='--')
 plt.yticks(np.arange(0, 110, 10))
 plt.ylim(0, 110)
     
-plt.legend()                                                            #Affiche les légendes associés à la courbe correspondante
-plt.margins(0, 0)                                                       #Force la disparition des marge intérieures
+plt.legend()                                                                    #Affiche les légendes associés à la courbe correspondante
+plt.margins(0, 0)                                                               #Force la disparition des marge intérieures
     
 #Défini les titres du graphes et des axes x et y
-plt.title(f"Etat des objectifs gouvernementaux pour la fin aout (Données du {format_date(nom_fichier[12:22])})")
+plt.title(f"Etat des objectifs gouvernementaux pour la fin aout (Données du {formatDate(nom_fichier[12:22])})")
 plt.xlabel("Dates")
 plt.ylabel("Pourcentage atteint des objectifs (%)")
     
