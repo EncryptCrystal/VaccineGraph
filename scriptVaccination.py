754c66102f54f7be2ef54204981dce2733f12240
Cyprien Méjat <adriendubois1411@gmail.com>
2021-08-14 00:28:19 +0200

    Ajouts de commentaires

--- scriptVaccination.py
+++ scriptVaccination.py
@@ -160,6 +160,8 @@ position_date_limite = len(liste_dates)-1                                       
 coeff = (proportion_vaccines[-1]-proportion_vaccines[-1-nb_jour_prediction])/nb_jour_prediction
 i = 0
 limite_atteinte = False
+#Si il n'y a pas de date limite de fin, alors créer des dates jusqu'à ce que les 100% de vaccinés soient atteints et/ou dépassés ET que la limite d'écart entre les dates soit respecté
+#Sinon, alors créer des dates jusqu'à ce que la date limite soit atteinte et/ou dépassée ET que la limite d'écart entre les dates soit respecté
 while (limite_date_fin == 0 and (i <= (100-proportion_vaccines[-1]) /coeff or ( len(liste_dates)-1) % limite_ecart_jour != 0) ) and (limite_atteinte == False or (len(liste_dates)-1)%limite_ecart_jour != 0):
     date = date[0:8] + str(int(date[8:])+1)
     if len(date[8:]) == 1: date = date[0:8] + "0" + date[-1] 
